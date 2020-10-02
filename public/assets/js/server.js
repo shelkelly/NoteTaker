a79e22c2b3e014dd8bb48a5882cdbf13cbe4af2b
Shelby <shelbypkelly@gmail.com>
2020-10-02 16:06:33 -0400

    server.js listening

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,7 +2,8 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var db = require("../db/db.json")
+
+let notedb = [];
 
 // set up express app
 var app = express();
@@ -24,30 +25,34 @@ app.get("/notes", function (req, res) {
 
 // API ROUTES
 app.get("/api/notes", function (req, res) {
-    notedb = fs.readFileSync("./Develop/db/db.json", "utf8");
+    notedb = fs.readFileSync("./db/db.json", "utf8");
     notedb = JSON.parse(notedb);
     res.json(notedb);
 });
 
 // CREATE NEW NOTE
 app.post("/api/notes", function (req, res) {
-    notedb = fs.readFileSync("./Develop/db/db.json", "utf8");
+    notedb = fs.readFileSync("./db/db.json", "utf8");
     notedb = JSON.parse(notedb);
     req.body.id = notedb.length;
     notedb.push(req.body);
     notedb = JSON.stringify(notedb);
-    fs.writeFile("./Develop/db/db.json", notedb, "utf8");
+    fs.writeFile("./db/db.json", notedb, "utf8");
     res.json(JSON.parse(notedb));
 })
 
 // DELETE NOTE
 app.delete("/api/notes/:id", function(req, res) {
-    notedb = fs.readFileSync("./Develop/db/db.json", "utf8");
+    notedb = fs.readFileSync("./db/db.json", "utf8");
     notedb = JSON.parse(notedb);
     notedb = notedb.filter(function(note) {
         return note.id != req.params.id;
     });
     notedb = JSON.stringify(notedb);
-    fs.writeFile("./Develop/db/db.json", notedb, "utf8");
+    fs.writeFile("./db/db.json", notedb, "utf8");
     res.send(JSON.parse(notedb));
 })
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
