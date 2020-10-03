d9655241c1256d702ccc723fbede1158697ee1d6
Shelby <shelbypkelly@gmail.com>
2020-10-02 20:48:55 -0400

    more file organization

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+//SERVER SETUP
+
 // dependencies
 const express = require("express");
 
-// set up express app
+// set up Express app
 const app = express();
 const PORT = process.env.PORT || 3005;
 
-// express app to handle data parsing
+// Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+require('./functioning.js')(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
