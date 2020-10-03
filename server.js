bc9a1b1b34adef58a19525c828b24d5f0f2ee9cc
Shelby <shelbypkelly@gmail.com>
2020-10-02 21:05:40 -0400

    complete file organization

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-require('./functioning.js')(app);
+require('./public/assets/js/functioning.js')(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
