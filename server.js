0d83ea34d4fd589f1e155f0135c08e2f8bafd6ab
Shelby <shelbypkelly@gmail.com>
2020-10-02 20:23:25 -0400

    removing commenting

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-//require("./public/assets/js/index.js");
+require("./public/assets/js/index.js");
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
